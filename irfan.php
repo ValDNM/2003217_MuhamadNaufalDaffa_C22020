b99abe5397442c7c375694fdc12ed6f2d286486d
ValDNM <82450154+ValDNM@users.noreply.github.com>
2022-03-17 13:38:08 +0700

    Delete irfan.php

--- irfan.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "=================================". "<br>";
-echo "Nama : Irfan Mochamad Esa". "<br>";
-echo "NIM : 2005568". "<br>" ;
-echo "Ilmu Komputer C2 2020". "<br>";
-echo "=================================". "<br>";
-
-?>
